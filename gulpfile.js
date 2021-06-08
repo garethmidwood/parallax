775dea9230c5deae4cfc738c558d5dd2340c0cfe
Gareth Midwood <gmidwood@gmail.com>
2021-06-08 23:52:26 +0100

    Remove commented code from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -28,15 +28,3 @@ exports.default = function() {
     watch('src/scss/**/*.scss', { ignoreInitial: false }, css);
     watch('src/js/**/*.js', { ignoreInitial: false }, series(clean, javascript));
 };
-
-//
-// gulp.task('css', () => {
-//     const postcss    = require('gulp-postcss')
-//     const sourcemaps = require('gulp-sourcemaps')
-//
-//     return gulp.src('src/**/*.scss')
-//         .pipe( sourcemaps.init() )
-//         .pipe( postcss([ require('precss'), require('autoprefixer') ]) )
-//         .pipe( sourcemaps.write('.') )
-//         .pipe( gulp.dest('build/') )
-// })
